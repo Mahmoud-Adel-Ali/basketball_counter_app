2f65b15ff0df3f482ca5952f1b2a1f5cd8d82041
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-01-24 19:50:23 -0800

    create function in cubit to increement team point

--- lib/cubit/couner_cubit.dart
+++ lib/cubit/couner_cubit.dart
@@ -2,8 +2,22 @@ import 'package:basketball_counter_app/cubit/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  
   CounterCubit() : super(CounterAIncreementState());
 
+  int teamAPoint = 0;
+  int teamBPoint = 0;
 
+  void teamIncreement({required String team, required int buttonNumber}) {
+    if (team == "A") {
+      //increement points
+      teamAPoint += buttonNumber;
+      //output state from cubit
+      emit(CounterAIncreementState());
+    } else {
+      //increement points
+      teamBPoint += buttonNumber;
+      //output state from cubit
+      emit(CounterBIncreementState());
+    }
+  }
 }
